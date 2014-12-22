fcf1157f8e7edb2ab26685c39f43315fdc0e7210
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2014-12-22 20:52:05 +0500

    Testing spring profiles

---
+++ src/test/java/ru/anr/base/tests/ProfilesFromPropertiesTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package ru.anr.base.tests;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ru.anr.base.BaseSpringParent;
+
+/**
+ * Tests for {@link BaseTestCase}
+ *
+ *
+ * @author Alexey Romanchuk
+ * @created 03 нояб. 2014 г.
+ *
+ */
+
+@Configuration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ProfilesFromPropertiesTest.class)
+@PropertySource("classpath:application.properties")
+public class ProfilesFromPropertiesTest extends BaseSpringParent {
+
+    /**
+     * Testing for profiles
+     */
+    @Test
+    public void testProfiles() {
+
+        Set<String> profs = getProfiles();
+        Assert.assertEquals(1, profs.size());
+        Assert.assertTrue(profs.contains("production"));
+
+        Environment e = getEnv();
+        Assert.assertNotNull(e);
+
+        Assert.assertEquals(set(e.getActiveProfiles()), profs);
+    }
+}
